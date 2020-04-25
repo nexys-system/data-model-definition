e15fd6b802b6848bc4acf8d6d6457c8259956238
Johan <johan@macair2020>
2020-04-25 15:10:46 +0200

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,6 @@ export default () => (<div className="container">
   <h1>DDL Checker</h1>
   <p>Use the menu to explore the different functionalities of the package</p>
 
-  <p><a href="https://github.com/Nexysweb/digis-ddl"><Icon name="code"/> Source</a> under MIT license</p>
+  <p><a href="https://github.com/Nexysweb/ddl"><Icon name="code"/> Source</a> under MIT license</p>
 </div>);
 
