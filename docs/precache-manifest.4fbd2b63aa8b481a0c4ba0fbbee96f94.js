d8ffc63d197817e9a3896754405ef2017c5f9429
Johan <johan@macair2020>
2020-04-25 15:37:27 +0200

    new publication

---
+++ docs/precache-manifest.4fbd2b63aa8b481a0c4ba0fbbee96f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8740ecdd9f5b82e575a80e2ebfe740a3",
+    "url": "/ddl/index.html"
+  },
+  {
+    "revision": "aa30a41715d9ec970793",
+    "url": "/ddl/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fad534ac8672b17a3da1",
+    "url": "/ddl/static/js/2.8b36f755.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ddl/static/js/2.8b36f755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa30a41715d9ec970793",
+    "url": "/ddl/static/js/main.bd9eb2b9.chunk.js"
+  },
+  {
+    "revision": "6a6a977627bd676c2c34",
+    "url": "/ddl/static/js/runtime-main.5660ecd1.js"
+  }
+]);
