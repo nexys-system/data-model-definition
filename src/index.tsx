f079741b8ae1582b905292590dec45171b16ca62
Johan <johan@macair2020>
2021-04-11 19:47:58 +0200

    add public url

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import RouterApp from "./router";
 
+const basename: string = import.meta.env.SNOWPACK_PUBLIC_URL || "";
+
 ReactDOM.render(
-  <Router>
+  <Router basename={basename}>
     <RouterApp />
   </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
