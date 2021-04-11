7a9c9ce4824b74f557c34927bfc918e04b1e8f86
johnb8005 <johnb8005@users.noreply.github.com>
2021-04-11 17:48:46 +0000

    deploy: f079741b8ae1582b905292590dec45171b16ca62

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
+
 import React from "./_snowpack/pkg/react.js";
 import ReactDOM from "./_snowpack/pkg/react-dom.js";
 import "./index.css.proxy.js";
 import {BrowserRouter as Router} from "./_snowpack/pkg/react-router-dom.js";
 import RouterApp from "./router.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(Router, null, /* @__PURE__ */ React.createElement(RouterApp, null)), document.getElementById("root"));
-serviceWorker.unregister();
+const basename = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_URL || "";
+ReactDOM.render(/* @__PURE__ */ React.createElement(Router, {
+  basename
+}, /* @__PURE__ */ React.createElement(RouterApp, null)), document.getElementById("root"));
