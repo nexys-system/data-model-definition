7c2455d8ed1f957703c18229c8ad35145ecc9e8c
Johan <johan@macair2020>
2021-04-11 19:58:53 +0200

    git sha

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import Icon from './components/icon';
-import { ghUrl } from './utils';
+import React from "react";
+import Icon from "./components/icon";
+import { ghUrl } from "./utils";
 
-export default () => (<div className="container">
-  <h1>Nexys Data Defintion Language</h1>
-  <p>Use the menu to explore the different functionalities of the package</p>
+const sha: string = import.meta.env.SNOWPACK_GIT_SHA || "sha_undefined";
+const ghSha = ghUrl + "/commit/" + sha;
 
-  <p><a href={ghUrl}><Icon name="code"/> Source</a> under MIT license</p>
-</div>);
+export default () => (
+  <div className="container">
+    <h1>Nexys Data Defintion Language</h1>
+    <p>Use the menu to explore the different functionalities of the package</p>
 
+    <p>
+      <a href={ghUrl}>
+        <Icon name="code" /> Source
+      </a>{" "}
+      under MIT license
+    </p>
+
+    <p>
+      <small>
+        <a href={ghSha}>{sha}</a>
+      </small>
+    </p>
+  </div>
+);
