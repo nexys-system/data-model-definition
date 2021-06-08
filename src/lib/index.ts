f3f27859cd072f03e4042697805301ff3e351beb
John B <john@nexys.ch>
2021-06-08 19:36:01 +0200

    export for lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,11 @@
-import * as SchemaValidation from './schema-validation';
-import * as JoiUtil from './joi-utils';
-import * as OpenApi from './open-api';
+import * as SchemaValidation from "./schema-validation";
+import * as JoiUtil from "./joi-utils";
+import * as OpenApi from "./open-api";
+import * as SQLTransform from "./sql";
 
 export default {
   SchemaValidation,
   JoiUtil,
-  OpenApi
+  OpenApi,
+  SQLTransform,
 };
